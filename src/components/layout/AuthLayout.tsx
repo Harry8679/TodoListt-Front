787856d252b302bf474ecd354ae88b4f7e1bd04f
Harry7986 <emarh.harry.code@gmail.com>
2026-01-29 23:57:47 +0100

    Auth Layout

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -2,16 +2,24 @@ import type { ReactNode } from "react";
 
 interface Props {
   title: string;
+  subtitle?: string; // ✅ AJOUT
   children: ReactNode;
 }
 
-const AuthLayout = ({ title, children }: Props) => {
+const AuthLayout = ({ title, subtitle, children }: Props) => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-indigo-600 to-purple-600 px-4">
       <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-8">
-        <h1 className="text-2xl font-bold text-center text-slate-800 mb-6">
+        <h1 className="text-2xl font-bold text-center text-slate-800">
           {title}
         </h1>
+
+        {subtitle && (
+          <p className="text-center text-sm text-slate-500 mt-2 mb-6">
+            {subtitle}
+          </p>
+        )}
+
         {children}
       </div>
     </div>
