cd7cf8fc0bf911be7984396290f1cf0b2eacb359
Harry7986 <emarh.harry.code@gmail.com>
2026-01-30 01:21:25 +0100

    Axios 2

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosError } from 'axios';
+import axios from 'axios';
+import type { AxiosInstance, InternalAxiosRequestConfig, AxiosError } from 'axios';
 
 // URL de base de l'API
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
