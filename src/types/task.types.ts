0ff0d8c43624ec23671adf7b6a2784d3dd1282cf
Harry7986 <emarh.harry.code@gmail.com>
2026-02-01 23:22:38 +0100

    Type Type 2

---
+++ src/types/task.types.ts
@@ -0,0 +1,11 @@
+// src/types/task.types.ts
+
+export interface Task {
+  _id: string;
+  title: string;
+  description?: string;
+  completed: boolean;
+  user: string;
+  createdAt: string;
+  updatedAt: string;
+}
